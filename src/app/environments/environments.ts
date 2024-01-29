288001fb1b706cb86505fbf7cab96494fde53b17
Hazem Safwat <HSafwat@moj.gov.sa>
2024-01-29 04:12:44 +0300

    init

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,32 +1,32 @@
 export const environment = {
-  production: false,
+    production: false,
     chatbotApiUrl: 'http://localhost:3500',
-  authOConfiguration: {
-    domain: 'dev-d3kvtws4.us.auth0.com',
-    clientId: 'tfqZAMepc4Cev1BkIrK4plFVOxjZALII',
-    authorizationParams: {
-      redirect_uri: window.location.origin,
-      // Request this audience at user authentication time
-      audience: 'http://localhost:3000',
-      // Request this scope at user authentication time
-      // scope: 'read:current_user',
-    },
-    // Specify configuration for the interceptor
-    httpInterceptor: {
-      allowedList: [
-        {
-          // Match any request that starts 'https://dev-d3kvtws4.us.auth0.com/api/v2/' (note the asterisk)
-          uri: 'http://localhost:3000/*',
-          tokenOptions: {
-            authorizationParams: {
-              // The attached token should target this audience
-              audience: 'http://localhost:3000',
-              // The attached token should have these scopes
-              // scope: 'read:current_user'
-            },
-          },
+    authOConfiguration: {
+        domain: 'dev-d3kvtws4.us.auth0.com',
+        clientId: 'tfqZAMepc4Cev1BkIrK4plFVOxjZALII',
+        authorizationParams: {
+            redirect_uri: window.location.origin,
+            // Request this audience at user authentication time
+            audience: 'http://localhost:3000',
+            // Request this scope at user authentication time
+            // scope: 'read:current_user',
         },
-      ],
+        // Specify configuration for the interceptor
+        httpInterceptor: {
+            allowedList: [
+                {
+                    // Match any request that starts 'https://dev-d3kvtws4.us.auth0.com/api/v2/' (note the asterisk)
+                    uri: 'http://localhost:3000/*',
+                    tokenOptions: {
+                        authorizationParams: {
+                            // The attached token should target this audience
+                            audience: 'http://localhost:3000',
+                            // The attached token should have these scopes
+                            // scope: 'read:current_user'
+                        },
+                    },
+                },
+            ],
+        },
     },
-  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
+import {environment} from "./app/environments/environments";
 
 bootstrapApplication(AppComponent, appConfig)
     .catch(err => console.error(err));
+
+console.log(environment.chatbotApiUrl)
